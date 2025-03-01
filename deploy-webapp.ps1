a87450964ddca5554a9a41002defde56f0519d8d
jsavage95 <jsav095@gmail.com>
2025-03-01 12:22:26 +1000

    add deploy-webapp code

---
+++ deploy-webapp.ps1
@@ -0,0 +1,19 @@
+param (
+    [string]$resourceGroupName = "HelloIOR-RG",
+    [string]$location = "australiaeast",
+    [string]$webAppName = "hello-ior-webapp-$(Get-Random -Maximum 10000)",
+    [string]$appServicePlanName = "HelloIOR-Plan"
+)
+
+# Login to Azure (handled by Azure DevOps service connection)
+# Create Resource Group
+az group create --name $resourceGroupName --location $location
+
+# Create App Service Plan (Free tier for cost-efficiency)
+az appservice plan create --name $appServicePlanName --resource-group $resourceGroupName --location $location --sku F1
+
+# Create Web App
+az webapp create --name $webAppName --resource-group $resourceGroupName --plan $appServicePlanName
+
+# Output Web App URL for reference
+Write-Output "Web App URL: https://$webAppName.azurewebsites.net"
